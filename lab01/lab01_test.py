58a617d7ba89fdc34f18e80aa07a26531d36446c
Chao <42952910+kenwan006@users.noreply.github.com>
2019-08-31 22:36:33 -0700

    Add files via upload

---
+++ lab01/lab01_test.py
@@ -0,0 +1,19 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 0)
+    1
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    """
+    "*** YOUR CODE HERE ***"
+    product = 1
+    while k > 0:
+        product = product * n 
+        n -= 1
+        k -= 1
+    return product
---
+++ lab06/vending_machine.py
@@ -0,0 +1,28 @@
+def vending_machine(snacks):
+    """Cycles through sequence of snacks.
+
+    >>> vender = vending_machine(('chips', 'chocolate', 'popcorn'))
+    >>> vender()
+    'chips'
+    >>> vender()
+    'chocolate'
+    >>> vender()
+    'popcorn'
+    >>> vender()
+    'chips'
+    >>> other = vending_machine(('brownie',))
+    >>> other()
+    'brownie'
+    >>> vender()
+    'chocolate'
+    """
+    "*** YOUR CODE HERE ***"
+    i = -1
+    def vender():
+        nonlocal i 
+        i += 1
+        if i >= len(snacks):
+            i = i - len(snacks)
+        return snacks[i]
+
+    return vender
